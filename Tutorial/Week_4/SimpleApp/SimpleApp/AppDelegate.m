2d8228e3d196915e8a36f6bdea94b57b7dd09272
Ben Ford <bentford@gmail.com>
2013-12-10 15:05:16 -0800

    Renamed to AppDelegate

--- Tutorial/Week_4/SimpleApp/SimpleApp/AppDelegate.m
+++ Tutorial/Week_4/SimpleApp/SimpleApp/AppDelegate.m
@@ -6,9 +6,9 @@
 //  Copyright (c) 2013 Instrument Marketing, Inc. All rights reserved.
 //
 
-#import "INAppDelegate.h"
+#import "AppDelegate.h"
 
-@implementation INAppDelegate
+@implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
--- Tutorial/Week_4/SimpleApp/SimpleApp/main.m
+++ Tutorial/Week_4/SimpleApp/SimpleApp/main.m
@@ -8,11 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-#import "INAppDelegate.h"
+#import "AppDelegate.h"
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([INAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
