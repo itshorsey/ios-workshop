63df85088a34f30dfc6fdfc21f99303de087a9a6
Ben Ford <bentford@gmail.com>
2014-01-29 09:15:54 -0800

    Added code from workshop.

--- Tutorial/Week_9/SimpleApp/SimpleApp/ViewController.m
+++ Tutorial/Week_9/SimpleApp/SimpleApp/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 // adopt private protocols here
-@interface ViewController ()
+@interface ViewController () <UIPickerViewDataSource, UIPickerViewDelegate>
 @end
 
 @implementation ViewController
@@ -31,16 +31,33 @@
     [super viewDidLoad];
 	
     
+    // create picker
+    UIPickerView *picker = [[UIPickerView alloc] initWithFrame:
+                            CGRectMake(0.0f, 0.0f, self.view.frame.size.width, 100.0f)];
+    picker.delegate = self;
+    picker.dataSource = self;
+    
+    [self.view addSubview:picker];
     
     
+    // create label
+    UILabel *instructionLabel = [[UILabel alloc] initWithFrame:CGRectMake(20.0f,
+                                                                          picker.frame.origin.y+picker.frame.size.height,
+                                                                          self.view.bounds.size.width, 300.0f)];
+    instructionLabel.text = @"Tap anywhere on the screen\n to show an alert view.";
+    // 0 indicates unlimited
+    instructionLabel.numberOfLines = 0;
+    [self.view addSubview:instructionLabel];
+    
+    
+    // create tap gesture
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTap:)];
+    [self.view addGestureRecognizer:tapGesture];
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    
-    
-    
     
 }
 
@@ -51,6 +68,63 @@
     
 }
 
+- (void)didTap:(UITapGestureRecognizer *)tapGesture
+{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Alert" message:@"message"
+                                                   delegate:self
+                                          cancelButtonTitle:@"Cancel"
+                                          otherButtonTitles:@"Option 1", @"Option 2", nil];
 
+    [alert show];
+}
+
+#pragma mark - UIPickerViewDelegate
+- (NSString *)pickerView:(UIPickerView *)pickerView
+             titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    if (row == 0)
+        return @"test test test";
+    
+    if (row == 1)
+        return @"another item";
+    
+    if (row == 2)
+        return @"some thing else";
+    
+    
+    return @"default choice";
+}
+#pragma mark -
+
+#pragma mark  UIPickerViewDatasource
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return 10;
+}
+#pragma mark -
+
+#pragma mark UIAlertViewDelegate
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"user tapped: %d", buttonIndex);
+}
+
+- (void)willPresentAlertView:(UIAlertView *)alertView
+{
+    NSLog(@"willPresentAlertView");
+}
+
+- (void)didPresentAlertView:(UIAlertView *)alertView
+{
+    NSLog(@"didPresentAlertView");
+}
+
+#pragma mark -
 
 @end
